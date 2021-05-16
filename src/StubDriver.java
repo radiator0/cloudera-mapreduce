4b283ebc265ee2a1ec0073b67b2e016fc209b539
Krzysztof Pilarczyk <radiator0@o2.pl>
2021-05-17 00:30:54 +0200

    Add input and output paths

--- src/StubDriver.java
+++ src/StubDriver.java
@@ -1,7 +1,10 @@
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class StubDriver {
@@ -40,6 +43,9 @@ public class StubDriver {
         job.setOutputValueClass(IntWritable.class);
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
         /*
          * TODO implement
