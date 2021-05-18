4e263bc07bca7827cb85a551c80b0dfb6ba2429f
matgd <matgd@users.noreply.github.com>
2021-05-18 23:17:17 +0200

    Fix

--- src/Stage2Mapper.java
+++ src/Stage2Mapper.java
@@ -1,68 +1,68 @@
+import org.apache.commons.collections.ArrayStack;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Stage2Mapper extends Mapper<LongWritable, Text, Text, Text> {
 
     @Override
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] splitted = Util.split(value);
-        if(splitted.length == 144) {
-            MapRajeevw(key, value, context, splitted);
-        } else if (splitted.length == 46) {
-            MapTheman(key, value, context, splitted);
-        } else {
-            System.err.println("Invalid row length: " + splitted.length);
-            System.err.println(value);
-        }
-    }
+        System.err.println(splitted);
 
-    private void MapRajeevw(LongWritable key, Text value, Context context, String[] splitted) throws IOException, InterruptedException {
-        if(splitted[0].equals("R_fighter")) {
-            return;
-        }
-        String winnerColor = splitted[5];
-        String winner = null;
-        String loser = null;
-        String referee = splitted[2];
-        if(winnerColor.equals("Draw")) {
-            if(splitted[0].compareTo(splitted[1]) > 0) {
-                winner = splitted[0];
-                loser = splitted[1];
-            } else {
-                winner = splitted[1];
-                loser = splitted[0];
-            }
-        } else {
-            if(winnerColor.equals("Red")) {
-                winner = splitted[0];
-                loser = splitted[1];
-            } else {
-                winner = splitted[1];
-                loser = splitted[0];
-            }
-        }
-        String newKey = winner + " vs " + loser + " ref " + referee;
-        String newValue = Util.join(new String[] {
-                winner, loser, referee, splitted[3], splitted[6], splitted[7], splitted[138], splitted[139], splitted[140], splitted[141]
-        });
-        context.write(new Text(newKey), new Text("1_" + newValue));
-    }
+        String winner = splitted[0].equals(splitted[4]) ? splitted[0] : splitted[1];
+        String loser = winner.equals(splitted[0]) ? splitted[1] : splitted[0];
+        String referee = splitted[3];
 
-    private void MapTheman(LongWritable key, Text value, Context context, String[] splitted) throws IOException, InterruptedException {
-        if(splitted[0].equals("Title")) {
-            return;
-        }
-        String winner = splitted[2];
-        String loser = splitted[1];
-        String referee = splitted[8];
+        String[] head_a = splitted[16].split(" ");
+        String[] head_b = splitted[17].split(" ");
+        String[] body_a = splitted[18].split(" ");
+        String[] body_b = splitted[19].split(" ");
+        String[] leg_a = splitted[20].split(" ");
+        String[] leg_b = splitted[21].split(" ");
+
+        String head_attempts_a = head_a.length == 3 ? head_a[2] : "";
+        String head_landed_a = head_a.length == 3 ? head_a[0] : "";
+        String head_attempts_b = head_b.length == 3 ? head_b[2] : "";
+        String head_landed_b = head_b.length == 3 ? head_b[0] : "";
+
+        String body_attempts_a = body_a.length == 3 ? body_a[2] : "";
+        String body_landed_a = body_a.length == 3 ? body_a[0] : "";
+        String body_attempts_b = body_b.length == 3 ? body_b[2] : "";
+        String body_landed_b = body_b.length == 3 ? body_b[0] : "";
+
+        String leg_attempts_a = leg_a.length == 3 ? leg_a[2] : "";
+        String leg_landed_a = leg_a.length == 3 ? leg_a[0]: "";
+        String leg_attempts_b = leg_b.length == 3 ? leg_b[2] : "";
+        String leg_landed_b = leg_b.length == 3 ? leg_b[0] : "";
+
 
         String newKey = winner + " vs " + loser + " ref " + referee;
-        String newValue = Util.join(new String[] {
-                winner, loser, referee
-        });
-        context.write(new Text(newKey), new Text("2_" + newValue));
+        ArrayList<String> newValueArrayList = new ArrayList<String>(Arrays.asList(splitted));
+        List<String> cutList = newValueArrayList.subList(0, 16);
+
+        cutList.add(head_attempts_a);
+        cutList.add(head_landed_a);
+        cutList.add(head_attempts_b);
+        cutList.add(head_landed_b);
+
+        cutList.add(body_attempts_a);
+        cutList.add(body_landed_a);
+        cutList.add(body_attempts_b);
+        cutList.add(body_landed_b);
+
+        cutList.add(leg_attempts_a);
+        cutList.add(leg_landed_a);
+        cutList.add(leg_attempts_b);
+        cutList.add(leg_landed_b);
+
+        String newValue = Util.join(cutList.toArray());
+        context.write(new Text(newKey), new Text(newValue));
     }
+
 }
