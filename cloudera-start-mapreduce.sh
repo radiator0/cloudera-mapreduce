7e58a0e344f4d1e52814512a35910e0360725b56
matgd <matgd@users.noreply.github.com>
2021-05-26 17:38:21 +0200

    Update cloudera-start-mapreduce.sh

--- cloudera-start-mapreduce.sh
+++ cloudera-start-mapreduce.sh
@@ -4,7 +4,7 @@ INPUT_PATH1="${2}"
 INPUT_PATH2="${3}"
 DATE=$(date "+%Y-%m-%d")
 
-/usr/bin/hadoop jar "${PATH_TO_JAR}" Stage1Driver INPUT_PATH1 INPUT_PATH2 /ufc/${DATE}/stage1  #  Stage 1
+/usr/bin/hadoop jar "${PATH_TO_JAR}" Stage1Driver "${INPUT_PATH1}" "${INPUT_PATH2}" /ufc/${DATE}/stage1  #  Stage 1
 /usr/bin/hadoop jar "${PATH_TO_JAR}" Stage2Driver /ufc/${DATE}/stage2-1 /ufc/${DATE}/stage2  #  Stage 2
 /usr/bin/hadoop jar "${PATH_TO_JAR}" Stage3Driver /ufc/${DATE}/stage3-1 /ufc/${DATE}/stage3  #  Stage 3
 /usr/bin/hadoop jar "${PATH_TO_JAR}" Stage4Driver /ufc/${DATE}/stage4-1 /ufc/${DATE}/stage4 /ufc/final  #  Stage 4
