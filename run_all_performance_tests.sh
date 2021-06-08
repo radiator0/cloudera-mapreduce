bbfd5d663723bdba98ffd2574dd652e48ba4e7b1
Krzysztof Pilarczyk <radiator0@o2.pl>
2021-06-09 00:22:02 +0200

    Add pattern of script for running tests

--- run_all_performance_tests.sh
+++ run_all_performance_tests.sh
@@ -1,2 +1,43 @@
 #!/bin/bash
-bash execute-test.sh /home/cloudera/Untitled.jar /testdata/rajeevw_5000.csv /testdata/theman_5000.csv
+
+FILE="all_performance_tests"
+
+echo >> FILE
+echo "Starting of tests..." >> FILE
+
+
+echo "@@ First group @@" >> FILE
+
+echo "Execution of rajeevw_5000.csv theman_5000.csv" >> FILE
+for i in {1..3}
+do
+  echo "# $i" >> FILE
+  start=`date +%s`
+  bash execute-test.sh /home/cloudera/Untitled.jar /testdata/rajeevw_5000.csv /testdata/theman_5000.csv
+  end=`date +%s`
+  runtime=$((end-start)) >> FILE
+done
+
+echo "Execution of rajeevw_25000.csv theman_25000.csv" >> FILE
+for i in {1..3}
+do
+  echo "# $i" >> FILE
+  start=`date +%s`
+  bash execute-test.sh /home/cloudera/Untitled.jar /testdata/rajeevw_25000.csv /testdata/theman_25000.csv
+  end=`date +%s`
+  runtime=$((end-start)) >> FILE
+done
+
+echo "Execution of rajeevw_50000.csv theman_50000.csv" >> FILE
+for i in {1..3}
+do
+  echo "# $i" >> FILE
+  start=`date +%s`
+  bash execute-test.sh /home/cloudera/Untitled.jar /testdata/rajeevw_50000.csv /testdata/theman_50000.csv
+  end=`date +%s`
+  runtime=$((end-start)) >> FILE
+done
+
+#TODO second group of tests
+
+echo "Tests finished" >> FILE
