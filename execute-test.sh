492ff00240344af1bae1142acb621b3dff0ba179
Krzysztof Pilarczyk <radiator0@o2.pl>
2021-06-09 13:28:18 +0200

    Add parameters to tests

--- execute-test.sh
+++ execute-test.sh
@@ -2,12 +2,13 @@
 PATH_TO_JAR="${1}"
 INPUT_PATH1="${2}"
 INPUT_PATH2="${3}"
+TEST_NAME="${4}"
 DATE=$(date "+%Y-%m-%d")
 DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"` #add %3N as we want millisecond too
 
 set -e
 
-/usr/bin/hadoop jar "${PATH_TO_JAR}" Stage1Driver "${INPUT_PATH1}" "${INPUT_PATH2}" /tests/${DATE_WITH_TIME}/ufc/${DATE}/stage1  #  Stage 1
-/usr/bin/hadoop jar "${PATH_TO_JAR}" Stage2Driver /tests/${DATE_WITH_TIME}/ufc/${DATE}/stage1 /tests/${DATE_WITH_TIME}/ufc/${DATE}/stage2  #  Stage 2
-/usr/bin/hadoop jar "${PATH_TO_JAR}" Stage3Driver /tests/${DATE_WITH_TIME}/ufc/${DATE}/stage2 /tests/${DATE_WITH_TIME}/ufc/${DATE}/stage3  #  Stage 3
-/usr/bin/hadoop jar "${PATH_TO_JAR}" Stage4Driver /tests/${DATE_WITH_TIME}/ufc/${DATE}/stage3 /tests/${DATE_WITH_TIME}/ufc/${DATE}/stage4 /tests/${DATE_WITH_TIME}/final  #  Stage 4
+/usr/bin/hadoop jar "${PATH_TO_JAR}" Stage1Driver "${INPUT_PATH1}" "${INPUT_PATH2}" /tests/${DATE_WITH_TIME}${TEST_NAME}/ufc/${DATE}/stage1  #  Stage 1
+/usr/bin/hadoop jar "${PATH_TO_JAR}" Stage2Driver /tests/${DATE_WITH_TIME}${TEST_NAME}/ufc/${DATE}/stage1 /tests/${DATE_WITH_TIME}${TEST_NAME}/ufc/${DATE}/stage2  #  Stage 2
+/usr/bin/hadoop jar "${PATH_TO_JAR}" Stage3Driver /tests/${DATE_WITH_TIME}${TEST_NAME}/ufc/${DATE}/stage2 /tests/${DATE_WITH_TIME}${TEST_NAME}/ufc/${DATE}/stage3  #  Stage 3
+/usr/bin/hadoop jar "${PATH_TO_JAR}" Stage4Driver /tests/${DATE_WITH_TIME}${TEST_NAME}/ufc/${DATE}/stage3 /tests/${DATE_WITH_TIME}${TEST_NAME}/ufc/${DATE}/stage4 /tests/${DATE_WITH_TIME}${TEST_NAME}/final  #  Stage 4
--- run_all_performance_tests.sh
+++ run_all_performance_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-FILE="all_performance_tests"
+DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"` #add %3N as we want millisecond too
+FILE="all_performance_tests$DATE_WITH_TIME"
 
 echo >> FILE
 echo "Starting of tests..." >> FILE
@@ -8,11 +9,11 @@ echo "Starting of tests..." >> FILE
 echo "@@ Prerun for prod data @@" >> FILE
 
 echo "Execution test for prod data" >> $FILE
-for i in {1..3}
+for i in {1..2}
 do
   echo "# $i" >> $FILE
   start=`date +%s`
-  bash execute-test.sh /home/cloudera/Untitled.jar /ufc/rajeevw_ufcdata/2021-05-17 /ufc/theman90210_ufc-fight-dataset/2021-05-17.csv
+  bash execute-test.sh /home/cloudera/Untitled.jar /ufc/rajeevw_ufcdata/2021-05-17 /ufc/theman90210_ufc-fight-dataset/2021-05-17.csv prod_data
   end=`date +%s`
   runtime=$((end-start))
   echo $runtime >> $FILE
@@ -21,11 +22,11 @@ done
 echo "@@ First group @@" >> FILE
 
 echo "Execution of rajeevw_5000.csv theman_5000.csv" >> $FILE
-for i in {1..3}
+for i in {1..2}
 do
   echo "# $i" >> $FILE
   start=`date +%s`
-  bash execute-test.sh /home/cloudera/Untitled.jar /testdata/rajeevw_5000.csv /testdata/theman_5000.csv
+  bash execute-test.sh /home/cloudera/Untitled.jar /testdata/rajeevw_5000.csv /testdata/theman_5000.csv test_both_5000
   end=`date +%s`
   runtime=$((end-start))
   echo $runtime >> $FILE
