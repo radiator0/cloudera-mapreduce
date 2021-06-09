29135ac9568cc2b84dca6535f5f83ac33aa30003
Krzysztof Pilarczyk <radiator0@o2.pl>
2021-06-09 13:17:32 +0200

    Test

--- run_all_performance_tests.sh
+++ run_all_performance_tests.sh
@@ -5,6 +5,18 @@ FILE="all_performance_tests"
 echo >> FILE
 echo "Starting of tests..." >> FILE
 
+echo "@@ Prerun for prod data @@" >> FILE
+
+echo "Execution test for prod data" >> $FILE
+for i in {1..3}
+do
+  echo "# $i" >> $FILE
+  start=`date +%s`
+  bash execute-test.sh /home/cloudera/Untitled.jar /ufc/rajeevw_ufcdata/2021-05-17 /ufc/theman90210_ufc-fight-dataset/2021-05-17.csv
+  end=`date +%s`
+  runtime=$((end-start))
+  echo $runtime >> $FILE
+done
 
 echo "@@ First group @@" >> FILE
 
@@ -19,27 +31,27 @@ do
   echo $runtime >> $FILE
 done
 
-echo "Execution of rajeevw_25000.csv theman_25000.csv" >> $FILE
-for i in {1..3}
-do
-  echo "# $i" >> $FILE
-  start=`date +%s`
-  bash execute-test.sh /home/cloudera/Untitled.jar /testdata/rajeevw_25000.csv /testdata/theman_25000.csv
-  end=`date +%s`
-  runtime=$((end-start))
-  echo $runtime >> $FILE
-done
+#echo "Execution of rajeevw_25000.csv theman_25000.csv" >> $FILE
+#for i in {1..3}
+#do
+#  echo "# $i" >> $FILE
+#  start=`date +%s`
+#  bash execute-test.sh /home/cloudera/Untitled.jar /testdata/rajeevw_25000.csv /testdata/theman_25000.csv
+#  end=`date +%s`
+#  runtime=$((end-start))
+#  echo $runtime >> $FILE
+#done
 
-echo "Execution of rajeevw_50000.csv theman_50000.csv" >> $FILE
-for i in {1..3}
-do
-  echo "# $i" >> $FILE
-  start=`date +%s`
-  bash execute-test.sh /home/cloudera/Untitled.jar /testdata/rajeevw_50000.csv /testdata/theman_50000.csv
-  end=`date +%s`
-  runtime=$((end-start))
-  echo $runtime >> $FILE
-done
+#echo "Execution of rajeevw_50000.csv theman_50000.csv" >> $FILE
+#for i in {1..3}
+#do
+#  echo "# $i" >> $FILE
+#  start=`date +%s`
+#  bash execute-test.sh /home/cloudera/Untitled.jar /testdata/rajeevw_50000.csv /testdata/theman_50000.csv
+#  end=`date +%s`
+#  runtime=$((end-start))
+#  echo $runtime >> $FILE
+#done
 
 #TODO second group of tests
 
