b94d3ef42a03a69c4cfce5b6ce94f4df589f57b0
Krzysztof Pilarczyk <radiator0@o2.pl>
2021-06-09 14:29:50 +0200

    Add all tests to script

--- run_all_performance_tests.sh
+++ run_all_performance_tests.sh
@@ -2,27 +2,28 @@
 
 DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"` #add %3N as we want millisecond too
 FILE="all_performance_tests$DATE_WITH_TIME"
+COUNT=1
 
 echo >> FILE
 echo "Starting of tests..." >> FILE
 
-echo "@@ Prerun for prod data @@" >> FILE
+#echo "@@ Prerun for prod data @@" >> FILE
 
-echo "Execution test for prod data" >> $FILE
-for i in {1..2}
-do
-  echo "# $i" >> $FILE
-  start=`date +%s`
-  bash execute-test.sh /home/cloudera/Untitled.jar /ufc/rajeevw_ufcdata/2021-05-17 /ufc/theman90210_ufc-fight-dataset/2021-05-17.csv prod_data
-  end=`date +%s`
-  runtime=$((end-start))
-  echo $runtime >> $FILE
-done
+#echo "Execution of test for prod data" >> $FILE
+#for i in {1..2}
+#do
+#  echo "# $i" >> $FILE
+#  start=`date +%s`
+#  bash execute-test.sh /home/cloudera/Untitled.jar /ufc/rajeevw_ufcdata/2021-05-17 /ufc/theman90210_ufc-fight-dataset/2021-05-17.csv prod_data
+#  end=`date +%s`
+#  runtime=$((end-start))
+#  echo $runtime >> $FILE
+#done
 
-echo "@@ First group @@" >> FILE
+echo "@@ First group - both files @@" >> FILE
 
-echo "Execution of rajeevw_5000.csv theman_5000.csv" >> $FILE
-for i in {1..2}
+echo "Execution of test with rajeevw_5000.csv and theman_5000.csv" >> $FILE
+for (( i=1; i<=$COUNT; i++ ))
 do
   echo "# $i" >> $FILE
   start=`date +%s`
@@ -32,28 +33,62 @@ do
   echo $runtime >> $FILE
 done
 
-#echo "Execution of rajeevw_25000.csv theman_25000.csv" >> $FILE
-#for i in {1..3}
-#do
-#  echo "# $i" >> $FILE
-#  start=`date +%s`
-#  bash execute-test.sh /home/cloudera/Untitled.jar /testdata/rajeevw_25000.csv /testdata/theman_25000.csv
-#  end=`date +%s`
-#  runtime=$((end-start))
-#  echo $runtime >> $FILE
-#done
+echo "Execution of test with rajeevw_25000.csv and theman_25000.csv" >> $FILE
+for (( i=1; i<=$COUNT; i++ ))
+do
+  echo "# $i" >> $FILE
+  start=`date +%s`
+  bash execute-test.sh /home/cloudera/Untitled.jar /testdata/rajeevw_25000.csv /testdata/theman_25000.csv test_both_25000
+  end=`date +%s`
+  runtime=$((end-start))
+  echo $runtime >> $FILE
+done
 
-#echo "Execution of rajeevw_50000.csv theman_50000.csv" >> $FILE
-#for i in {1..3}
-#do
-#  echo "# $i" >> $FILE
-#  start=`date +%s`
-#  bash execute-test.sh /home/cloudera/Untitled.jar /testdata/rajeevw_50000.csv /testdata/theman_50000.csv
-#  end=`date +%s`
-#  runtime=$((end-start))
-#  echo $runtime >> $FILE
-#done
+echo "Execution of test with rajeevw_50000.csv and theman_50000.csv" >> $FILE
+for (( i=1; i<=$COUNT; i++ ))
+do
+  echo "# $i" >> $FILE
+  start=`date +%s`
+  bash execute-test.sh /home/cloudera/Untitled.jar /testdata/rajeevw_50000.csv /testdata/theman_50000.csv test_both_50000
+  end=`date +%s`
+  runtime=$((end-start))
+  echo $runtime >> $FILE
+done
 
-#TODO second group of tests
+
+echo "@@ Second group - only one file @@" >> FILE
+
+echo "Execution of test with rajeevw_5000.csv and EMPTY" >> $FILE
+for (( i=1; i<=$COUNT; i++ ))
+do
+  echo "# $i" >> $FILE
+  start=`date +%s`
+  bash execute-test.sh /home/cloudera/Untitled.jar /testdata/rajeevw_5000.csv /testdata/emptyfile test_OF_5000
+  end=`date +%s`
+  runtime=$((end-start))
+  echo $runtime >> $FILE
+done
+
+echo "Execution of test with rajeevw_25000.csv and EMPTY" >> $FILE
+for (( i=1; i<=$COUNT; i++ ))
+do
+  echo "# $i" >> $FILE
+  start=`date +%s`
+  bash execute-test.sh /home/cloudera/Untitled.jar /testdata/rajeevw_25000.csv /testdata/emptyfile test_OF_25000
+  end=`date +%s`
+  runtime=$((end-start))
+  echo $runtime >> $FILE
+done
+
+echo "Execution of test with rajeevw_50000.csv and EMPTY" >> $FILE
+for (( i=1; i<=$COUNT; i++ ))
+do
+  echo "# $i" >> $FILE
+  start=`date +%s`
+  bash execute-test.sh /home/cloudera/Untitled.jar /testdata/rajeevw_50000.csv /testdata/emptyfile test_OF_50000
+  end=`date +%s`
+  runtime=$((end-start))
+  echo $runtime >> $FILE
+done
 
 echo "Tests finished" >> $FILE
--- upload-data-to-hdfs.sh
+++ upload-data-to-hdfs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 hadoop fs -mkdir /testdata
+hadoop fs -touchz /testdata/emptyfile
 hadoop fs -put test-data/rajeevw_5000.csv /testdata
 hadoop fs -put test-data/rajeevw_25000.csv /testdata
 hadoop fs -put test-data/rajeevw_50000.csv /testdata
