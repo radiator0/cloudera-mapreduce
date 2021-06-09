f13d75835f167e5c2172ca89d0f12dc72ecd36f1
Krzysztof Pilarczyk <radiator0@o2.pl>
2021-06-09 17:21:10 +0200

    Logging in script

--- run_all_performance_tests.sh
+++ run_all_performance_tests.sh
@@ -4,10 +4,10 @@ DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"` #add %3N as we want millisecond too
 FILE="all_performance_tests$DATE_WITH_TIME"
 COUNT=2
 
-echo >> FILE
-echo "Starting of tests..." >> FILE
+echo >> $FILE
+echo "Starting of tests..." >> $FILE
 
-echo "@@ First group - both files @@" >> FILE
+echo "@@ First group - both files @@" >> $FILE
 
 echo "Execution of test with rajeevw_5000.csv and theman_5000.csv" >> $FILE
 for (( i=1; i<=$COUNT; i++ ))
